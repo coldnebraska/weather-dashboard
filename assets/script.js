65e648ee0bb51efd98ce167933a9e336e9e7a891
DBrush <coldnebraska@gmail.com>
2023-10-20 14:24:13 -0500

    fixed function layout

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 function getApi() {
   let requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=Austin&appid=6cddf7d816b2147ea014ff6c5dd1dbeb'
-// {city} {key}
+  // {city} {key}
   requestUrl.replace("city", "Austin")
   requestUrl.replace("key", "6cddf7d816b2147ea014ff6c5dd1dbeb")
   // console.log (requestUrl)
@@ -15,51 +15,53 @@ function getApi() {
       setCurrentWeather(data)
       setForecastWeather(data)
     });
+}
+
+function setCurrentWeather(data) {
+  let selectedCity = $(".city")
+  let currentIcon = $("#current-weather-icon")
+  let currentTemp = $("#current-temp")
+  let currentWind = $("#current-wind")
+  let currentHumidity = $("#current-humidity")
+  
+  selectedCity.text(data.city.name)
+  currentTemp.text(Math.floor(data.list[0].main.temp * 9 / 5 - 459.67))
+  currentWind.text(data.list[0].wind.speed)
+  currentHumidity.text(data.list[0].main.humidity)
+
+  if (data.list[0].weather[0].id === 800) { // clear
+    currentIcon.addClass("bi bi-sun-fill")
+  } else if (data.list[0].weather[0].id > 800) { //cloudy
+    currentIcon.addClass("bi bi-cloud-fill")
+  } else if (data.list[0].weather[0].id < 800 && data.list[0].weather[0].id >= 700) { // haze/mist/fog/dust
+    currentIcon.addClass("bi bi-cloud-haze")
+  } else if (data.list[0].weather[0].id < 700 && data.list[0].weather[0].id >= 600) { // snow
+    currentIcon.addClass("bi bi-snow")
+  } else if (data.list[0].weather[0].id < 500 && data.list[0].weather[0].id >= 300) { // rain/drizzle
+    currentIcon.addClass("bi bi-cloud-rain-heavy-fill")
+  } else { // thunderstorm
+    currentIcon.addClass("bi bi-cloud-lightning-fill")
+  }
+}
+
+function setForecastWeather(data) {
+  let x = 7
+
+  for (i = 1; i <= 5; i++) {
+    let forecastTemp = $(".forecast").children().eq(i - 1).children().eq(2).children()
+    forecastTemp.text(Math.floor(data.list[x].main.temp * 9 / 5 - 459.67))
+
+    let forecastWind = $(".forecast").children().eq(i - 1).children().eq(3).children()
+    forecastWind.text(data.list[x].wind.speed)
+
+    let forecastHumidity = $(".forecast").children().eq(i - 1).children().eq(4).children()
+    forecastHumidity.text(data.list[x].main.humidity)
+
+    x += 8
   }
 
-  function setCurrentWeather(data) {
-    let selectedCity = $(".city")
-    let currentIcon = $("#current-weather-icon")
-    let currentTemp = $("#current-temp")
-    let currentWind = $("#current-wind")
-    let currentHumidity = $("#current-humidity")
-    
-    selectedCity.text(data.city.name)
-    currentTemp.text(Math.floor(data.list[0].main.temp * 9 / 5 - 459.67))
-    currentWind.text(data.list[0].wind.speed)
-    currentHumidity.text(data.list[0].main.humidity)
-
-    if (data.list[0].weather[0].id === 800) { // clear
-      currentIcon.addClass("bi bi-sun-fill")
-    } else if (data.list[0].weather[0].id > 800) { //cloudy
-      currentIcon.addClass("bi bi-cloud-fill")
-    } else if (data.list[0].weather[0].id < 800 && data.list[0].weather[0].id >= 700) { // haze/mist/fog/dust
-      currentIcon.addClass("bi bi-cloud-haze")
-    } else if (data.list[0].weather[0].id < 700 && data.list[0].weather[0].id >= 600) { // snow
-      currentIcon.addClass("bi bi-snow")
-    } else if (data.list[0].weather[0].id < 500 && data.list[0].weather[0].id >= 300) { // rain/drizzle
-      currentIcon.addClass("bi bi-cloud-rain-heavy-fill")
-    } else { // thunderstorm
-      currentIcon.addClass("bi bi-cloud-lightning-fill")
-    }
-  }
-
-  function setForecastWeather(data) {
-    // set forecast temp
-    let x = 7
-    for (i = 1; i <= 5; i++) {
-      let forecastTemp = $(".forecast").children().eq(i - 1).children().eq(2).children()
-      forecastTemp.text(Math.floor(data.list[x].main.temp * 9 / 5 - 459.67))
-
-      let forecastWind = $(".forecast").children().eq(i - 1).children().eq(3).children()
-      forecastWind.text(data.list[x].wind.speed)
-
-      let forecastHumidity = $(".forecast").children().eq(i - 1).children().eq(4).children()
-      forecastHumidity.text(data.list[x].main.humidity)
-      x += 8
-    }
-  }
-
+  setForecastIcons()
+}
 
 function getDate() {
   let date = new Date()
@@ -71,7 +73,7 @@ function getDate() {
   curDate.text(currentDate)
 }
 
-function setIcons() {
+function setForecastIcons() {
   let day1Icon = $("#day1-icon")
   let day2Icon = $("#day2-icon")
   let day3Icon = $("#day3-icon")
@@ -86,5 +88,4 @@ function setIcons() {
 }
 
 getDate()
-setIcons()
 getApi()
